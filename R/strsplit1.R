d60ade9add8e210a7a2516ae7977409565faaff9
spedunkler <jerry@sfsu.edu>
2021-11-26 15:42:37 -0800

    namespace, description, license, etc.

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,3 +1,12 @@
+#' strsplit1
+#'
+#' @param x string to split
+#' @param split character to split with
+#'
+#' @return split into a vector
+#' @export
+#'
+#' @examples
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
