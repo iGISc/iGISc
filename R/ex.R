07ff8205d8856e8f64493474c138b65c224bfdf9
spedunkler <jerry@sfsu.edu>
2021-11-26 22:37:31 -0800

    added ex.Rd

---
+++ R/ex.R
@@ -0,0 +1,13 @@
+#' Reading in extdata from package
+#'
+#' @param dta file to read (.csv, .xlsx, .shp, etc.)
+#'
+#' @return path to file
+#' @export
+#'
+#' @examples
+#' read.csv(ex("sierra/sierraStations.csv"))
+ex <- function(dta){
+  system.file("extdata",dta,package="iGIScData")
+}
+
