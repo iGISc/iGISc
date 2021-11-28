1951bec05cbfc0bc42ae72e97dcd2298f54f2234
spedunkler <jerry@sfsu.edu>
2021-11-28 12:31:20 -0800

    TRI data documentation removed

---
+++ inst/dataDocRemoved.R
@@ -0,0 +1,35 @@
+#' EPA Toxic Release Inventory data, 1987, CA
+#'
+#' EPA Toxic Release Inventory (TRI) for 1987, sites in California.
+#'
+#' @format data frame with 6,378 entries, 110 variables (see the web page)
+#'
+#' @source:\url{https://www.epa.gov/toxics-release-inventory-tri-program/tri-data-and-tools}
+"TRI_1987_CA"
+
+#' EPA Toxic Release Inventory data, 1997, CA
+#'
+#' EPA Toxic Release Inventory (TRI) for 1997, sites in California.
+#'
+#' @format data frame with 4,198 entries, 110 variables (see the web page)
+#'
+#' @source:\url{https://www.epa.gov/toxics-release-inventory-tri-program/tri-data-and-tools}
+"TRI_1997_CA"
+
+#' EPA Toxic Release Inventory data, 2007, CA
+#'
+#' EPA Toxic Release Inventory (TRI) for 2007, sites in California.
+#'
+#' @format data frame with 4,492 entries, 110 variables (see the web page)
+#'
+#' @source:\url{https://www.epa.gov/toxics-release-inventory-tri-program/tri-data-and-tools}
+"TRI_2007_CA"
+
+#' EPA Toxic Release Inventory data, 2017, CA
+#'
+#' EPA Toxic Release Inventory (TRI) for 2017, sites in California.
+#'
+#' @format data frame with 3,685 entries, 110 variables (see the web page)
+#'
+#' @source:\url{https://www.epa.gov/toxics-release-inventory-tri-program/tri-data-and-tools}
+"TRI_2017_CA"
