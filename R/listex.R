e88acf8417c178d666b261b5b85e987f91b6c140
spedunkler <jerry@sfsu.edu>
2022-10-21 10:08:26 -0700

    added listex.R (not installed)

---
+++ R/listex.R
@@ -0,0 +1,26 @@
+#' Listing contents of extdata from package
+#'
+#' @return path to file
+#' @export
+#'
+#' @examples
+#'
+library(igisci); library(stringr)
+for(d in list.dirs(ex(""),recursive=F)){
+  #print(d)
+  dsplit <- strsplit(d,"/")
+  len <- length(dsplit[[1]])
+  writeLines(paste("----DIR:",dsplit[[1]][len]))
+  for(shp in list.files(d,pattern="*.shp$")){
+    print(shp)
+  }
+  for(tif in list.files(d,pattern="*.tif$")){
+    print(tif)
+  }
+  for(csv in list.files(d,pattern="*.csv")){
+    print(csv)
+  }
+  for(xls in list.files(d,pattern="*.xls")){
+    print(csv)
+  }
+}
