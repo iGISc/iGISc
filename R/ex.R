7181f159c8db3381e1fd922ecfaedbbf2bbe0a1b
spedunkler <jerry@sfsu.edu>
2022-10-22 09:34:38 -0700

    Added exfiles() to ex()

--- R/ex.R
+++ R/ex.R
@@ -10,4 +10,34 @@
 ex <- function(dta){
   system.file("extdata",dta,package="igisci")
 }
+#' Listing contents of extdata from package
+#' @param NA
+#' @return tibble
+#' @export
+#'
+#' @examples
+#' view(exfiles())
+exfiles <- function(){
+  library(dplyr)
+  exfilesDF <- tribble(~dir,~file,~type)
+  for(d in list.dirs(ex(""),recursive=F)){
+    #print(d)
+    dsplit <- strsplit(d,"/")
+    len <- length(dsplit[[1]])
+    dir <- dsplit[[1]][len]
+    for(shp in list.files(d,pattern="*.shp$")){
+      exfilesDF <- bind_rows(exfilesDF,tribble(~dir,~file,~type,dir,shp,"shapefile"))
+    }
+    for(tif in list.files(d,pattern="*.tif$")){
+      exfilesDF <- bind_rows(exfilesDF,tribble(~dir,~file,~type,dir,tif,"TIFF"))
+    }
+    for(csv in list.files(d,pattern="*.csv")){
+      exfilesDF <- bind_rows(exfilesDF,tribble(~dir,~file,~type,dir,csv,"CSV"))
+    }
+    for(xls in list.files(d,pattern="*.xls")){
+      exfilesDF <- bind_rows(exfilesDF,tribble(~dir,~file,~type,dir,xls,"xls"))
+    }
+  }
+  exfilesDF
+}
 
